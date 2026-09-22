0c5546ccbbed20ded227c8bdc4aa1bcfaf516a7b
081796 <kschuhl@polytechnic.org>
2026-09-21 20:24:40 -0700

    Update config.js

--- principal-parts/config.js
+++ principal-parts/config.js
@@ -1,2 +1,2 @@
 // Paste the Google Apps Script web-app URL between the quotation marks.
-window.SESSION_LOG_URL = "";
+window.SESSION_LOG_URL = "https://script.google.com/macros/s/AKfycbyDjIo_45FBE_hLKKU1RLbrSssXX-8C4xUK_L3t3TxGeNl_Z6NnefzXCMkf6XTNKgvQ4Q/exec";
